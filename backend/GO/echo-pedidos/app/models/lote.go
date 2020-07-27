1a03b74159e9e052677b54cb1331dbf831025351
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-07-27 02:26:42 -0400

    pedidos

--- backend/GO/echo-pedidos/app/models/lote.go
+++ backend/GO/echo-pedidos/app/models/lote.go
@@ -2,7 +2,7 @@ package models
 
 //Lote .
 type Lote struct {
-	tableName         struct{} `pg:"kmr_pedido_lote"`
+	tableName         struct{} `pg:"kmr_pedido_lote" sql:"kmr_pedido_lote"`
 	ID                int      `pg:"id"`
 	IDPedido          int      `pg:"id_pedido"`
 	IDIngPresentacion int      `pg:"id_ing_prsentacion"`
